008340c8f8ec52d65bb84ebe51e78ef6a44d068d
Sreenath285 <sreenath.285@gmail.com>
2022-03-05 20:54:44 +0530

    finding size of the tree

---
+++ BinarySearchTree/src/Main.java
@@ -0,0 +1,49 @@
+public class Main {
+
+    /***
+     * main method for manipulation
+     * @param args - default param(not used)
+     */
+    public static void main(String[] args) {
+
+        /***
+         * PROCEDURE
+         * 1. creating binary tree service object
+         * 2. creating my node
+         * 3. inserting data to binary search tree
+         * 4. printing binary search tree in preorder
+         * 5. printing size of the tree
+         */
+
+        /***
+         * 1. creating binary tree service
+         */
+        BinaryTreeService binaryTreeService = new BinaryTreeService();
+
+        /***
+         * 2. creating my node and init to null
+         */
+        MyNode myNode = null;
+
+        /***
+         * 3. inserting data to binary search tree
+         */
+        myNode = binaryTreeService.insertNode(myNode,56);
+        myNode = binaryTreeService.insertNode(myNode,30);
+        myNode = binaryTreeService.insertNode(myNode,70);
+        myNode = binaryTreeService.insertNode(myNode,36);
+        myNode = binaryTreeService.insertNode(myNode,78);
+        myNode = binaryTreeService.insertNode(myNode,64);
+
+        /***
+         * 4. printing binary search tree in preorder
+         */
+        binaryTreeService.preOrder(myNode);
+
+        /***
+         * 5. printing size of the tree
+         */
+        System.out.println("Size of the tree : " + binaryTreeService.size(myNode));
+
+    }
+}
